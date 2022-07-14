d6dac2a65d05228a71f9be708f0bde1921fafbbf
fancn21th <fancn21th@gmail.com>
2022-07-15 06:30:22 +0800

    inquirer

---
+++ src/index.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+import chalk from "chalk";
+import chalkAnimation from "chalk-animation";
+
+async function main() {
+  const rainbowTitle = chalkAnimation.rainbow("Hello World! 范轶洁");
+}
+
+await main();
