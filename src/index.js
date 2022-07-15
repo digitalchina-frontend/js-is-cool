c9b7fd4eaa55d74b001178ceedb7a4d41fc4e092
fancn21th <fancn21th@gmail.com>
2022-07-15 10:43:57 +0800

    简单的问题

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import chalk from "chalk";
 import chalkAnimation from "chalk-animation";
 
 async function main() {
-  const rainbowTitle = chalkAnimation.rainbow("Hello World! 范轶洁");
+  const rainbowTitle = chalkAnimation.rainbow("你喜欢学习前端吗?");
 }
 
 await main();
